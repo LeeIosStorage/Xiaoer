534d510a5de1c0a7eb20b00afc9c1ea3a29f96c4
Leejun <823974987@qq.com>
2015-03-06 17:29:48 +0800

    评测webView (禁止滑动返回)

---
+++ Xiaoer/ViewControllers/Evaluation/XEWebViewWithEvaluationVc.h
@@ -0,0 +1,13 @@
+//
+//  XEWebViewWithEvaluationVc.h
+//  Xiaoer
+//
+//  Created by KID on 15/3/6.
+//
+//
+
+#import "XECommonWebVc.h"
+
+@interface XEWebViewWithEvaluationVc : XECommonWebVc
+
+@end
